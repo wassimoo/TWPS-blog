4b865bc060e489511ee1f409cf36bdcf3c5af81e
wassimoo <wassimjbaali5121997@gmail.com>
2018-02-08 05:36:14 +0100

    new article fixed

--- src/controllers/httpRequests/updateContent.php
+++ src/controllers/httpRequests/updateContent.php
@@ -24,7 +24,11 @@ class updateContent
             if (!isset($_POST["id"]) || !isset($_POST["title"]) || !isset($_POST["content"]) || !isset($_POST["cover"])) {
                 return false;
             } else {
-                $_SESSION["id"] = $_POST["id"];
+                $_POST["title"] = trim(preg_replace('/\s+/', ' ',$_POST["title"]));
+                $_POST["title"] = strtolower($_POST["title"]);
+                //if http origins is new , ignore given id ; 
+                //TODO : replace with server domain
+                $_SESSION["id"] = $_SERVER["HTTP_REFERER"] == "http://localhost/blog/new" ? str_replace(' ','-',$_POST["title"]) :  $_POST["id"];
                 $_SESSION["title"] = $_POST["title"];
                 $_SESSION["content"] = $_POST["content"];
                 $_SESSION["coverLink"] = $_POST["cover"];
@@ -40,12 +44,12 @@ class updateContent
             // verify if article already exists
             $query = "SELECT count(id) FROM article where id = ?";
             $rows = Queries::performQuery($dbh, $query, array($_SESSION["id"]), "select");
-            if ($rows[0][0] != 1) {
+            if ($rows[0][0] != 1){
                 //insert new article ;
                 $query = "INSERT INTO article(id,content,admin_username,title,banner_link) values(?,?,?,?,?)";
                 $data = array($_SESSION["id"], $_SESSION["content"], $_SESSION["username"], $_SESSION["title"], $_SESSION["coverLink"]);
                 $type = "insert";
-            } else {
+            }else {
                 //update article ;
                 $query = "UPDATE twps.article SET content = ?, title= ?, banner_link= ? WHERE id = ?";
                 $data = array($_SESSION["content"], $_SESSION["title"], $_SESSION["coverLink"], $_SESSION["id"]);
