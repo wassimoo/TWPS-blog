483a1be1ae695f39b9cb7bdd1f49718ffea3b7e7
wassimoo <wassimjbaali5121997@gmail.com>
2018-02-13 10:27:49 +0100

    admin tools added.

--- src/assets/js/admin/admin.js
+++ src/assets/js/admin/admin.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+
+    var active1 = false;
+    var active2 = false;
+    var active3 = false;
+    var active4 = false;
+
+    $('.parent2').on('mousedown touchstart', function () {
+
+        if (!active1) $(this).find('.logout').css({ 'background-color': 'gray', 'transform': 'translate(0px,125px)' });
+        else $(this).find('.logout').css({ 'background-color': 'dimGray', 'transform': 'none' });
+        if (!active2) $(this).find('.med').css({ 'background-color': 'gray', 'transform': 'translate(-60px,105px)' });
+        else $(this).find('.med').css({ 'background-color': 'darkGray', 'transform': 'none' });
+        if (!active3) $(this).find('.new').css({ 'background-color': 'gray', 'transform': 'translate(-105px,60px)' });
+        else $(this).find('.new').css({ 'background-color': 'silver', 'transform': 'none' });
+        if (!active4) $(this).find('.upload').css({ 'background-color': 'gray', 'transform': 'translate(-125px,0px)' });
+        else $(this).find('.upload').css({ 'background-color': 'silver', 'transform': 'none' });
+        active1 = !active1;
+        active2 = !active2;
+        active3 = !active3;
+        active4 = !active4;
+
+    });
+
     $("#saveContent").click(function (e) {
         e.preventDefault();
         var id = document.location.href.match(/[^\/]+$/)[0];
