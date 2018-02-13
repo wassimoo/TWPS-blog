576e7509ce05655141fc4527aa707204455ec101
wassimoo <wassimjbaali5121997@gmail.com>
2018-02-13 12:03:07 +0100

    spin Loader added.

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -2,6 +2,8 @@
 $(document).ready(function () {
     var active = null;
     function fetchData() {
+        $(".m-blog_listing").css({"display":"none"});
+        $(".parent").css({"display":"block"});
         var pageNum = this.id ? this.id : 1;
         if(active)
             $(active).attr("class","nav");
@@ -17,17 +19,17 @@ $(document).ready(function () {
             },
             dataType: "html",
             success: function (html) {
+                $(".parent").css({"display":"none"});
                 if (html != "false") {
                     $(".m-blog_listing").html(html);
                 }
                 else {
-                    $(".m-blog_listing").innerHTML = "Cette service n'est pas disponible è ce moment<br> Veuillez ressayer dans quelques instants.";
+                    $(".m-blog_listing").html("Cette service n'est pas disponible è ce moment<br> Veuillez ressayer dans quelques instants.");
                 }
+                $(".m-blog_listing").css({"display":""});
             }
         })
     }
     fetchData();
     $(document).on("click", ".nav", fetchData);
 });
-
-
--- src/controllers/httpRequests/updateContent.php
+++ src/controllers/httpRequests/updateContent.php
@@ -26,7 +26,7 @@ class updateContent
             } else {
                 $_POST["title"] = trim(preg_replace('/\s+/', ' ',$_POST["title"]));
                 //if http origins is new , ignore given id ; 
-                //TODO : replace with server domain
+                //TODO: replace with server domain
                 $_SESSION["id"] = $_SERVER["HTTP_REFERER"] == "http://localhost/blog/new" ? str_replace(' ','-',strtolower($_POST["title"])) :  $_POST["id"];
                 $_SESSION["title"] = $_POST["title"];
                 $_SESSION["content"] = $_POST["content"];
